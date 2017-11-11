a382c1206f644406d88339d34b1d33e7155c4b62
759748407 <759748407@qq.com>
2017-11-11 16:48:37 +0800

    修改换进配置

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from "vue";
+import cube from 'cube-ui';
 import App from "./App";
 import { createRouter } from "./router";
 import api from "./api";
 import http from "./http";
 
+Vue.use(cube);
 Vue.config.productionTip = false;
 Vue.prototype.$api = api;
 Vue.prototype.$axios = http;
